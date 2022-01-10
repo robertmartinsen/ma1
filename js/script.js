f89b5854f72106d52f7d351d3e3d3100d8dcd672
robertmartinsen <robertmartinsen2000@gmail.com>
2022-01-10 22:11:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 //oppgave 1.
-var integer = 7;
+var number = 7;
 var isLoggedIn = true;
 var myName ="Robert";
 
 //oppgave 2.
 var myName = "Robert";
 var myLastName = "Martinsen";
-var myFullName = myName + " " + myLastName;
+var myName = myName + " " + myLastName;
 console.log(myName);
 
 //oppgave 3.
-var animal = "frog";
+var name = "frog";
+console.log(typeof name);
+//the type of frog is string
+
+//oppgave 4.
+var orderHasShipped = true;
+if(orderHasShipped === true) {
+    console.log("The order shipped");
+}
+else{
+    console.log("The order did not ship");
+}
+
+//oppgave 5.
+for(var counter = 7; i <= 13; i++) {
+    console.log(counter);
+}

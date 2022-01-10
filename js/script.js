070c33e301ff102765588db623867cee5e9ade27
robertmartinsen <robertmartinsen2000@gmail.com>
2022-01-10 21:21:28 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//oppgave 1.
+var integer = 7;
+var isLoggedIn = true;
+var myName ="Robert";
+
+//oppgave 2.
+var myName = "Robert";
+var myLastName = "Martinsen";
+var myFullName = myName + " " + myLastName;
+console.log(myName);
+
+//oppgave 3.
+var animal = "frog";
